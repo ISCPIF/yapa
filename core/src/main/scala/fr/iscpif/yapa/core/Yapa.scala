6357c4d34a7fae12fe3cff92b597fef07c357841
Martin Degenne <martin.degenne@gmail.com>
2013-06-11 18:01:49 +0200

    SshObject start definition

--- core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
+++ core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
@@ -1,4 +1,23 @@
 package fr.iscpif.yapa.core
 
-class Yapa {
+import net.schmizz.sshj.SSHClient;
+
+class SshObject {
+  val ssh = new SSHClient();
+  def connection(host:String, user:String, mdp:String) = {
+    ssh.loadKnownHosts();
+    ssh.connect(host);
+    try {
+      ssh.authPassword(user, mdp);
+      val session = ssh.startSession();
+      print("Seem legit");
+      session.close();
+    } finally { ssh.disconnect(); }
+  }
+
+}
+
+object Yapa extends App {
+  val ssh = new SshObject();
+  ssh.connection("localhost", "martin-port", "");
 }
