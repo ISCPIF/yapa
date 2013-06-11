a2a50f76d7bba29e7ef2cc59584e20b425810fc1
Martinovsky <martin.degenne@gmail.com>
2013-06-11 15:49:46 +0200

    Change dependencies

--- project/YapaBuild.scala
+++ project/YapaBuild.scala
@@ -10,9 +10,9 @@ object YapaBuild extends Build {
 
  lazy val yapa = Project(id = "yapa", base = file("yapa"))
 
- lazy val core = Project(id = "core", base = file("core")) dependsOn(yapa)
+ lazy val core = Project(id = "core", base = file("core")) settings (libraryDependencies ++= Seq("net.schmizz" % "sshj" % "0.1.1")) dependsOn(yapa)
 
- lazy val gui = Project(id = "gui", base = file("gui")) settings (libraryDependencies ++= Seq("org.scala-lang" % "scala-swing" % "2.10.1", "net.schmizz" % "sshj" % "0.1.1")) dependsOn(yapa, uri("git://github.com/roguePanda/java-terminal.git#88eb0e64670adcb112af14832cdc57721b94af14"))
+ lazy val gui = Project(id = "gui", base = file("gui")) settings (libraryDependencies ++= Seq("org.scala-lang" % "scala-swing" % "2.10.1")) dependsOn(yapa, core, uri("git://github.com/roguePanda/java-terminal.git#88eb0e64670adcb112af14832cdc57721b94af14"))
 
  lazy val all = Project(id = "all", base = file(""))  settings (publish := { }) dependsOn(yapa, core, gui) aggregate(yapa, core, gui)
 }
