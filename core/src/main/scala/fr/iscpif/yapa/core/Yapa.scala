0604a38343778c77e68b2f12a0f01407861ebf6d
Martin Degenne <martin.degenne@gmail.com>
2013-06-12 16:02:22 +0200

    Connection VM working

--- core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
+++ core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
@@ -1,28 +1,34 @@
 package fr.iscpif.yapa.core
 
-import net.schmizz.sshj.SSHClient
+import net.schmizz.sshj._
 import net.schmizz.sshj.transport.verification.HostKeyVerifier
 import java.security.PublicKey
 ;
 
 class SshObject {
+
   val ssh = new SSHClient
   ssh.addHostKeyVerifier(new HostKeyVerifier {
     def verify(p1: String, p2: Int, p3: PublicKey) = true })
-  def connection(host:String, user:String, mdp:String) = {
+  ssh.setConnectTimeout(0)
+  ssh.setTimeout(0)
+
+  def connection(host:String, port:Int, user:String, mdp:String) = {
     ssh.loadKnownHosts
-    ssh.connect(host)
+    println("Try connection")
+    ssh.connect(host, port)
     try {
       ssh.authPassword(user, mdp)
       val session = ssh.startSession
       println("Seem legit")
       session.close
-    } finally { ssh.disconnect }
+    } finally { ssh.disconnect
+    println("finally")}
   }
 
 }
 
 object Yapa extends App {
   val ssh = new SshObject
-  ssh.connection("localhost", "yapa", "yapa")
+  ssh.connection("localhost", 2222, "yapa", "yapa")
 }
