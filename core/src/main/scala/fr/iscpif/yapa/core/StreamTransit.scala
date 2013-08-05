cd617d587a1fedd75c9d4ef15539c94269109f3a
Martin Degenne <martin.degenne@gmail.com>
2013-08-05 20:13:53 +0200

    modif transit (histo + suppr)

--- core/src/main/scala/fr/iscpif/yapa/core/StreamTransit.scala
+++ core/src/main/scala/fr/iscpif/yapa/core/StreamTransit.scala
@@ -3,11 +3,12 @@ package fr.iscpif.yapa.core
 import net.schmizz.sshj.common.StreamCopier
 import java.io.{PipedOutputStream, PipedInputStream, OutputStream}
 import net.schmizz.sshj.connection.channel.direct.Session
+import java.util
 
 class StreamTransit {
-  val output = new PipedOutputStream()
-  val input = new PipedInputStream(output)
 
+  val tab = new  util.Vector[util.Vector[Byte]](1)
+   this.tab.add(new util.Vector[Byte](1))
 
   def show (shell : Session.Shell) {
     var read : Int = 0
@@ -21,31 +22,53 @@ class StreamTransit {
     val buf = new Array[Byte](1)
     try
     {
-      var test = true
-    while ((my_read(buf) != -1) && test)
+      var test = new  util.Vector[Boolean](4)
+      test.clear()
+      test.add(true)
+      test.add(false)
+      test.add(false)
+      test.add(false)
+      var up = 1
+      val clear = "\b \b"
+    while (test.elementAt(0) && (my_read(buf) != -1))
     {
+      test.remove(3)
+      test.add(3, false)
+      buf.foreach(_ match {
+        case 3 => {test.remove(0)
+        test.add(0, false)}
+        case 4 => {test.remove(0)
+          test.add(0, false)}
+        case 10 => { if (this.tab.elementAt(0).capacity > 1)
+        {this.tab.add(0, new util.Vector[Byte](1))}
+        up = 1}
+        case 27 => {test.remove(1)
+          test.add(1, true)}
+        case 91 => {if (test.elementAt(1))
+        {test.remove(1)
+        test.add(1, false)
+        test.remove(2)
+        test.add(2, true)}}
+        case 65 => {if (test.elementAt(2))
+        {test.remove(2)
+          test.add(2, false)
+          test.remove(3)
+          test.add(3, true)
+          print(clear*4)
+          if (up < (tab.capacity - 1))
+          {
+          print(tab.elementAt(up))
+          up = (up + 1)
+        }}}
+        case 127 => print(clear*3)
+        case _ =>
+        })
+      if (buf(0) != 10 && buf(0) != 127 && !(test.elementAt(1) || test.elementAt(2) || test.elementAt(3))) {
+        this.tab.elementAt(0).add(buf(0))}
       shell.getOutputStream.write(buf, 0, read)
       shell.getOutputStream.flush()
-      //val str = buf.map(_.toInt)
-       buf.foreach(print)
-      buf.foreach(_ match {
-        case 0x3 => test = false
-        case 0x4 => test = false
-        case 0x8 => print("\b \b")
-        case _ =>
-        //case x: Any => println("other : " + x)
-        })
-
+      //println("")
     }}
-    catch {case e: Exception => }
-       /* i match {
-        case -1 =>
-        case 3 => test = false
-        case 4 => test = false
-        case 8 => print("\b \b")
-        case 13 =>
-        case _ =>
-
-    }*/
+    catch {case e: Exception => println(e)}
   }
 }
