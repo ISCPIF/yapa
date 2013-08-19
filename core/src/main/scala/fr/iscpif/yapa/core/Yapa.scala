98683c990fed0685b06439c2ca6227a5df4bdfe8
Martin Degenne <martin.degenne@gmail.com>
2013-08-19 19:32:44 +0200

    Choice VM (modif main)

--- core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
+++ core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
@@ -1,14 +1,44 @@
 package fr.iscpif.yapa.core
 
+import java.io.File
+
 object Yapa extends App {
-  val vm = new VM("qemu-system-x86_64", "/media/martin-port/test/DDV.img")
-  val p = vm.process
-  Thread.sleep(5000)
-  val ssh = new SshObject("localhost", 2222, "yapa", "yapa")
-  try {
-  ssh.chat
+
+  def recursiveListFiles(f: File): Array[File] = {
+    val these = f.listFiles
+    these ++ these.filter(_.isDirectory).flatMap(recursiveListFiles)
   }
-  finally { println("prog end")
-    p.destroy()
+  try {
+  val choice = (new File("/media/martin-port/test/")).listFiles()
+  println("which one do you want ?")
+  var i = 0
+  choice.foreach(x => {
+    println(i+"/ "+x.getName)
+    i = i + 1
+  })
+  println(i+"/ Enter the VM's path")
+  var f = readInt()
+  while (f< 0 || f > i)
+  {println("wrong number. Which one do you want ?")
+  f = readInt()}
+
+  val path = choice(f).getAbsolutePath
+  val file = new File(path);
+  if(file.exists()) {
+    start(path)
+  }}
+  catch {case e: Exception => }
+
+  def start(path : String) = {
+    val vm = new VM("qemu-system-x86_64", path)
+    val p = vm.process
+    Thread.sleep(5000)
+    val ssh = new SshObject("localhost", 2222, "yapa", "yapa")
+    try {
+      ssh.chat
+    }
+    finally { println("prog end")
+      p.destroy()
+    }
   }
 }
