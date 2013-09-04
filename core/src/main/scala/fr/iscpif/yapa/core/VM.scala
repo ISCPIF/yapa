54072dda6548f8dfaeba6c643b5d08a7f1ce439f
Martin Degenne <martin.degenne@gmail.com>
2013-09-04 18:33:56 +0200

    Clean and modif JCterm

--- core/src/main/scala/fr/iscpif/yapa/core/VM.scala
+++ core/src/main/scala/fr/iscpif/yapa/core/VM.scala
@@ -2,12 +2,29 @@ package fr.iscpif.yapa.core
 
 import scala.sys.process.Process
 
-class VM(pathq:String, path:String) {
+class VM() {
 
-  lazy val paths = path
-  lazy val pathqs = pathq
-  println(paths)
-  def process = {
-    Process(pathqs+" "+paths+" -redir tcp:2222::22 -nographic").run()
+  var paths = ""
+  var pathqs = ""
+  var vm : Process = new Process {
+    def exitValue(): Int = 0
+
+    def destroy() {}
+  }
+
+  def startVM = {
+    vm = Process(pathqs+" "+paths+" -redir tcp:2222::22 -nographic").run()
+  }
+
+  def stopVM = {
+    vm.destroy()
+  }
+
+  def setQemuPath(path : String) = {
+    pathqs = path
+  }
+
+  def setVMPath(path : String) = {
+    paths = path
   }
 }
--- core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
+++ core/src/main/scala/fr/iscpif/yapa/core/Yapa.scala
@@ -4,49 +4,61 @@ import java.io.File
 import java.util
 
 object Yapa extends App {
+  val tab = new util.Vector[File]
+  var pathVM = ""
 
   try {
   val choice = (new File("/media/martin-port/test/")).listFiles()
-  val tab = new util.Vector[File]
-  println("which one do you want ?")
   choice.foreach(x => {
     if ((x.getName).contains(".img"))
     {tab.add(x)}})
-  for (i <- 0 to (tab.size() - 1))
-  {
-    println(i+"/ "+tab.elementAt(i).getName)
+    test()
   }
-  println(tab.size()+"/ Enter the VM's path")
-  var f = readInt()
-  while (f< 0 || f > tab.size())
-  {println("wrong number. Which one do you want ?")
-  f = readInt()}
-  var path = ""
-  if (f != tab.size()) {
-   path = tab.elementAt(f).getAbsolutePath
-  }
-  else {
-    println("path ?")
-    path = readLine()
-  }
-  val file = new File(path);
-  if(file.exists()) {
-    start(path)
-  }}
   catch {case e: Exception => }
 
-  def start(path : String) = {
-    val vm = new VM("qemu-system-x86_64", path)
-    val p = vm.process
-    Thread.sleep(5000)
-    val ssh = new SshObject("localhost", 2222, "yapa", "yapa")
+  def setQemuPath(path : String) = {
+    Global.vm.setQemuPath(path)
+  }
+
+  def test() = {
+    choseVM("/media/martin-port/test/DDV.img")
+     start
+  }
+
+  def choseVM(index : Int) : Boolean = {
+     if (index< 0 || index > (tab.size() - 1))
+       return false
+    else {
+       pathVM = tab.elementAt(index).getAbsolutePath
+     }
+    return true
+  }
+
+  def choseVM(path : String) : Boolean = {
+    val file = new File(path)
+    if(!file.exists())
+      return false
+    else {
+      pathVM = path
+    }
+    return true
+  }
+
+  def seeVMavaible() : util.Vector[String] = {
+    val rval = new util.Vector[String]
+    for (i <- 0 to (tab.size() - 1))
+    {
+      rval.add(tab.elementAt(i).getName)
+    }
+   return rval
+  }
+
+  def start() = {
+
     try {
-      //ssh.chat
       JCTermSwingFrame.main(new Array[String](0))
-      println("This is the END")
     }
     finally { println("prog end")
-      p.destroy()
     }
   }
 }
